e234a544484349af282cc69bf89879d36f419a14
Simon Penel <simon.penel@univ-lyon1.fr>
2026-06-15 16:11:23 +0200

    Add docker stuff

---
+++ docker/docker_postgis/fill_table_arbres_suite.sql
@@ -0,0 +1,48 @@
+INSERT INTO arbres (nom, nom_site, annee, prod_gland,geom)
+VALUES (
+    'Chene 1',
+    'Parc',
+    2002,
+    110,
+    ST_SetSRID(ST_MakePoint(4.87, 45.76), 4326)
+);
+INSERT INTO arbres (nom, nom_site, annee, prod_gland,geom)
+VALUES (
+    'Chene 1',
+    'Parc',
+    2003,
+    10,
+    ST_SetSRID(ST_MakePoint(4.87, 45.76), 4326)
+);
+INSERT INTO arbres (nom, nom_site, annee, prod_gland,geom)
+VALUES (
+    'Chene 1',
+    'Parc',
+    2004,
+    500,
+    ST_SetSRID(ST_MakePoint(4.87, 45.76), 4326)
+);
+INSERT INTO arbres (nom, nom_site, annee, prod_gland,geom)
+VALUES (
+    'Chene 1',
+    'Parc',
+    2005,
+    3,
+    ST_SetSRID(ST_MakePoint(4.87, 45.76), 4326)
+);
+INSERT INTO arbres (nom, nom_site, annee, prod_gland,geom)
+VALUES (
+    'Chene 1',
+    'Parc',
+    2006,
+    100,
+    ST_SetSRID(ST_MakePoint(4.87, 45.76), 4326)
+);
+INSERT INTO arbres (nom, nom_site, annee, prod_gland,geom)
+VALUES (
+    'Chene 1',
+    'Parc',
+    2007,
+    25,
+    ST_SetSRID(ST_MakePoint(4.87, 45.76), 4326)
+);
