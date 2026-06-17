d477af85e753e1f0c7bf3673b08ce5a58e718c2c
Simon Penel <simon.penel@univ-lyon1.fr>
2026-06-17 15:09:15 +0200

    Using addGeoJSONv2

--- docker/docker_shiny/app.R
+++ docker/docker_shiny/app.R
@@ -1,27 +1,49 @@
 library(shiny)
 library(leaflet)
+library(leaflet.extras)
+library(shinyjs)
 
-url <- "http://node:8080/api/arbres"
-geojson <- jsonlite::fromJSON(url,
-  simplifyVector = TRUE,
-  simplifyDataFrame = TRUE)
-print("DEBUG GEOJSON =")
-gdf <- geojson$features
-new_df <- data.frame(
-  arbre = gdf$propertie$nom,
-  site = gdf$propertie$nom_site,
-  latitude  =sapply(gdf$geometry$coordinates, `[`, 1),
-  longitude  =sapply(gdf$geometry$coordinates, `[`, 2),
-  year = gdf$propertie$annee
-)
-print(new_df)
+# url <- "http://node:8080/api/arbres"
+# geojson <- jsonlite::fromJSON(url,
+#   simplifyVector = TRUE,
+#   simplifyDataFrame = TRUE)
+# print("DEBUG GEOJSON =")
+# gdf <- geojson$features
+# new_df <- data.frame(
+#   arbre = gdf$propertie$nom,
+#   site = gdf$propertie$nom_site,
+#   latitude  =sapply(gdf$geometry$coordinates, `[`, 1),
+#   longitude  =sapply(gdf$geometry$coordinates, `[`, 2),
+#   year = gdf$propertie$annee
+# )
+# print(new_df)
 
 ui <- fluidPage(
+  useShinyjs(),
+  tags$script(HTML("
+    Shiny.addCustomMessageHandler('loadData', function(url) {
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          // Send data back to Shiny
+          Shiny.setInputValue('geojson_data', data);
+        })
+        .catch(error => console.error(error));
+    });
+  ")),
   titlePanel("Simple Leaflet Map"),
   leafletOutput("map", height = 500)
 )
 
 server <- function(input, output, session) {
+
+  # Trigger the fetch call
+  observe({
+    session$sendCustomMessage(
+      "loadData",
+      "http://127.0.0.1:8080/api/arbres"
+    )
+  })
 
   output$map <- renderLeaflet({
     leaflet() %>%
@@ -34,8 +56,11 @@ server <- function(input, output, session) {
       addMarkers(
         lng = 2.3522,
         lat = 48.8566,
-        popup = "Hello from Paris!"
-      )
+        popup = "Hello from Paris!",
+        clusterOptions = markerClusterOptions()
+      ) %>%
+      addGeoJSONv2(input$geojson_data,clusterOptions = markerClusterOptions()) 
+      # clusterOptions = markerClusterOptions()
   })
 
 }
