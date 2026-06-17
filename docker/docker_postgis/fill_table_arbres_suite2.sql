1757104de6a3af1c7e26ad1d9ec2575d4272def8
Simon Penel <simon.penel@univ-lyon1.fr>
2026-06-17 16:09:14 +0200

    Add fill_table_arbres_suite2.sql

---
+++ docker/docker_postgis/fill_table_arbres_suite2.sql
@@ -0,0 +1,48 @@
+INSERT INTO arbres (nom, nom_site, annee, prod_gland,geom)
+VALUES (
+    'Chene 2',
+    'Parc',
+    2002,
+    110,
+    ST_SetSRID(ST_MakePoint(4.87, 45.86), 4326)
+);
+INSERT INTO arbres (nom, nom_site, annee, prod_gland,geom)
+VALUES (
+    'Chene 2',
+    'Parc',
+    2003,
+    10,
+    ST_SetSRID(ST_MakePoint(4.87, 45.86), 4326)
+);
+INSERT INTO arbres (nom, nom_site, annee, prod_gland,geom)
+VALUES (
+    'Chene 2',
+    'Parc',
+    2004,
+    500,
+    ST_SetSRID(ST_MakePoint(4.87, 45.86), 4326)
+);
+INSERT INTO arbres (nom, nom_site, annee, prod_gland,geom)
+VALUES (
+    'Chene 2',
+    'Parc',
+    2005,
+    3,
+    ST_SetSRID(ST_MakePoint(4.87, 45.86), 4326)
+);
+INSERT INTO arbres (nom, nom_site, annee, prod_gland,geom)
+VALUES (
+    'Chene 2',
+    'Parc',
+    2006,
+    100,
+    ST_SetSRID(ST_MakePoint(4.87, 45.86), 4326)
+);
+INSERT INTO arbres (nom, nom_site, annee, prod_gland,geom)
+VALUES (
+    'Chene 2',
+    'Parc',
+    2007,
+    25,
+    ST_SetSRID(ST_MakePoint(4.87, 45.86), 4326)
+);
